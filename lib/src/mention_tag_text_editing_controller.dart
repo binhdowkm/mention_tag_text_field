506b51972e540249d747e821081647ae355eebfb
binhdowkm <binhdo@wakumo.vn>
2024-08-07 16:03:31 +0700

    feature: remove all mentions

--- lib/src/mention_tag_text_editing_controller.dart
+++ lib/src/mention_tag_text_editing_controller.dart
@@ -60,6 +60,12 @@ class MentionTagTextEditingController extends TextEditingController {
     }
   }
 
+  void removeAllMentions() {
+    while (_mentions.isNotEmpty) {
+      remove(index: 0);
+    }
+  }
+
   late MentionTagDecoration mentionTagDecoration;
   void Function(String?)? onMention;
 
